da9874598a607046d1677e30f524175e72c9148f
utkarrshhh <utkarsh3639@gmail.com>
2024-03-03 12:06:27 +0530

    some mnogo docs

---
+++ temp2.js
@@ -0,0 +1,35 @@
+db.runCommand({
+  collMod: "nonfiction",
+  validator: {
+    $jsonSchema: {
+      required: ["name", "price", "author"],
+      properties: {
+        name: {
+          bsonType: "string",
+          description: "must be a string and required",
+        },
+        price: {
+          bsonType: "number",
+          description: "must be a number and required",
+        },
+        author: {
+          bsonType: "array",
+          description: "must be an array and required",
+          items: {
+            bsonType: "object",
+            required: ["name", "email"],
+            properties: {
+              name: {
+                bsonType: "string",
+              },
+              email: {
+                bsonType: "string",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  validationAction: "warn",
+});
